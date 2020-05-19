fcf6a15dc7b13f77180e046c8557195c421ed8c3
Hu2y <sanghun_@daum.net>
2020-05-19 15:48:58 +0900

    [#10] Feat: Vue-Router 설치 및 인스턴스 주입, 라우팅 설정
     - 초기 페이지는 login페이지로 리다이렉트 시켜주었다.
     - 로그인이 완료되면 main페이지로 넘기려고 한다

---
+++ FE/huey/src/routes/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/main',
+      component: () => import('@/views/MainView.vue'),
+    },
+  ],
+});
+
+export default router;
