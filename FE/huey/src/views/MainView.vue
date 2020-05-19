7eb0a2801015b045110d2e15e599d23b783d1b12
Hu2y <sanghun_@daum.net>
2020-05-19 15:52:20 +0900

    [#10] Feat: 로그인이 완료되고 이동할 페이지 생성
     - MainView 파일을 생성해주었다
     - 필터처리를 할 버튼을 컴포넌트로 만들어서 랜더링 시켜주었다

---
+++ FE/huey/src/views/MainView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <date-filter-button-component></date-filter-button-component>
+  </div>
+</template>
+
+<script>
+import DateFilterButtonComponent from '@/components/DateFilterButtonComponent';
+
+export default {
+  components: {
+    DateFilterButtonComponent,
+  },
+};
+</script>
+
+<style></style>
