7ee8f76c389f945dbea349bb4fb9ed51581afc6a
Hu2y <sanghun_@daum.net>
2020-05-19 14:08:02 +0900

    [#8] Feat: Login View를 App Component에 연결

--- FE/huey/src/App.vue
+++ FE/huey/src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    test
+    <login-view></login-view>
   </div>
 </template>
 
 <script>
-export default {};
+import LoginView from '@/views/LoginView';
+
+export default {
+  components: {
+    LoginView,
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
