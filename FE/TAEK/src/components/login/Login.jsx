40151af6da87a9c7f08b3dc4f27e868e609f2187
sgdg <seungdeng17@gmail.com>
2020-05-21 18:51:11 +0900

    Chore: 로그인 oauth 링크 비활성화

--- FE/TAEK/src/components/login/Login.jsx
+++ FE/TAEK/src/components/login/Login.jsx
@@ -76,8 +76,8 @@ const Login = () => {
                 <AirbnbLogo />
             </LogoWrap>
             <LoginBtn>
-                {/* <Link to='/main'>로그인</Link> */}
-                <a href='https://github.com/login/oauth/authorize?client_id=c2e25fe082feb25b3a02&redirect_uri=http://15.164.35.235/api/github/oauth/callback2&scope=user'>로그인</a>
+                <Link to='/main'>로그인</Link>
+                {/* <a href='https://github.com/login/oauth/authorize?client_id=c2e25fe082feb25b3a02&redirect_uri=http://15.164.35.235/api/github/oauth/callback2&scope=user'>로그인</a> */}
             </LoginBtn>
         </LoginWrap>
     )
