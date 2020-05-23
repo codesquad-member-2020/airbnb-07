5760536c7cfe60314342e499ba1d10e3ed65a5b2
Hu2y <sanghun_@daum.net>
2020-05-24 00:45:57 +0900

    [#42] Chore: Lazy 로딩을 위한 컴포넌트 틀 작성

---
+++ FE/huey/src/components/LazyLoading/ImageItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="img-item">
+    이미지 아이템
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageItem',
+  props: {},
+};
+</script>
---
+++ FE/huey/src/components/LazyLoading/ImageList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="img-list">
+    <ImageItem />
+  </div>
+</template>
+
+<script>
+import ImageItem from './ImageItem';
+
+export default {
+  name: 'ImageList',
+  components: {
+    ImageItem,
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
