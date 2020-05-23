b218cb167c1c7956737aed085ce2d08c2b38994e
sgdg <seungdeng17@gmail.com>
2020-05-23 20:12:56 +0900

    [#35] Feat: 요금 선택 리셋 action 추가

--- FE/TAEK/src/store/modules/charge/chargeReducer.js
+++ FE/TAEK/src/store/modules/charge/chargeReducer.js
@@ -1,28 +1,35 @@
-import { RESET_CHARGE, MIN_CHARGE, MAX_CHARGE } from './chargeAction';
+import { SAVE_CHARGE, RESET_CHARGE, MIN_CHARGE, MAX_CHARGE } from './chargeAction';
 import { MAIN } from 'constants/constant';
 
 const initialState = {
+    isSave: false,
     min: MAIN.CHARGE.MIN_CHARGE,
     max: MAIN.CHARGE.MAX_CHARGE,
 }
 
 export default function chargeReducer(state = initialState, action) {
     switch (action.type) {
+        case SAVE_CHARGE:
+            return {
+                ...state,
+                isSave: true,
+            }
         case RESET_CHARGE:
             return {
                 ...state,
                 ...initialState,
             }
         case MIN_CHARGE:
-
             return {
                 ...state,
                 min: action.payload,
+                isSave: false,
             }
         case MAX_CHARGE:
             return {
                 ...state,
                 max: action.payload,
+                isSave: false,
             }
         default:
             return state
