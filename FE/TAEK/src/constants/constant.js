00f394f20725f236af9ee6e7706b3cb393c8abc1
sgdg <seungdeng17@gmail.com>
2020-05-23 17:30:30 +0900

    [#35] Feat: 요금 상수값 추가

--- FE/TAEK/src/constants/constant.js
+++ FE/TAEK/src/constants/constant.js
@@ -13,6 +13,10 @@ const MAIN = {
         CHILD_AGE_TEXT: '2 ~ 12세',
         BABY_TEXT: '유아',
         BABY_AGE_TEXT: '2세 미만',
+    },
+    CHARGE: {
+        MIN_CHARGE: 12000,
+        MAX_CHARGE: 1000000,
     }
 }
 
