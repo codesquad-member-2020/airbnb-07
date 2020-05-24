0d3db3211c0ba357580b5ba0cbb74cb6c2336b9e
sgdg <seungdeng17@gmail.com>
2020-05-24 19:18:41 +0900

    [#45] Feat: 날짜 저장, 리셋 action, reducer 추가

--- FE/TAEK/src/store/modules/date/dateReducer.js
+++ FE/TAEK/src/store/modules/date/dateReducer.js
@@ -1,21 +1,38 @@
-import { CHECK_IN, CHECK_OUT } from './dateAction';
+import { SAVE_DATE, RESET_DATE, CHECK_IN, CHECK_OUT } from './dateAction';
 
 const initialState = {
+    isSave: false,
+    isChange: false,
     checkInDate: null,
     checkOutDate: null,
 }
 
 export default function dateReducer(state = initialState, action) {
     switch (action.type) {
+        case SAVE_DATE:
+            return {
+                ...state,
+                isSave: true,
+                isChange: false,
+            }
+        case RESET_DATE:
+            return {
+                ...state,
+                ...initialState,
+            }
         case CHECK_IN:
             return {
                 ...state,
                 checkInDate: action.payload,
+                isSave: false,
+                isChange: true,
             }
         case CHECK_OUT:
             return {
                 ...state,
                 checkOutDate: action.payload,
+                isSave: false,
+                isChange: true,
             }
         default:
             return state
