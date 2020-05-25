52207def47b129f2f97ba04dcde35ef73aac84c6
sgdg <seungdeng17@gmail.com>
2020-05-26 00:00:42 +0900

    Style: gird-template-columns 속성값 수정

--- FE/TAEK/src/components/main/rooms/RoomList.jsx
+++ FE/TAEK/src/components/main/rooms/RoomList.jsx
@@ -5,13 +5,13 @@ import RoomCard from './RoomCard';
 const RoomListWrap = styled.div`
     margin-top : 40px;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: 340px;
     grid-gap: 35px;
 
-    @media ( max-width: 1680px ) { grid-template-columns: 1fr 1fr 1fr; }
-    @media ( max-width: 1180px ) { grid-template-columns: 1fr 1fr; }
-    @media ( max-width: 680px ) { grid-template-columns: 1fr; }
+    @media ( max-width: 1680px ) { grid-template-columns: repeat(3, 1fr); }
+    @media ( max-width: 1180px ) { grid-template-columns: repeat(2, 1fr); }
+    @media ( max-width: 680px ) { grid-template-columns: repeat(1, 1fr); }
 `;
 
 const RoomList = ({ allData }) => {
