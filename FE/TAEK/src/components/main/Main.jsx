1deb53872a1d5991afbff087f2e64bb761c59351
sgdg <seungdeng17@gmail.com>
2020-05-25 13:09:30 +0900

    Chore: 숙소 필터 버튼 컴포넌트 추가

--- FE/TAEK/src/components/main/Main.jsx
+++ FE/TAEK/src/components/main/Main.jsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
-import MainHeader from '@/components/main/MainHeader';
-import DateFilter from '@/components/main/filterButtons/date/DateFilter';
-import PersonFilter from '@/components/main/filterButtons/person/PersonFilter';
-import ChargeFilter from '@/components/main/filterButtons/charge/ChargeFilter';
+import MainHeader from './header/MainHeader';
+import FilterButtons from './filterButtons/FilterButtons';
 
 const MainContentsWrap = styled.div`
     padding: 30px 10%;
-`;
-
-const FilterBtnWrap = styled.div`
-    display: flex;
-    position: relative;
-    height: 39.98px;
-    &::after {
-        content: '';
-        position: absolute;
-        width: 100%;
-        border-bottom: 0.5px solid #bdc3c7;
-        top: calc(100% + 15px);
-        left: 0;
-    }
-    > * {
-       margin-right: 20px;
-    }
 `;
 
 const Main = () => {
@@ -31,11 +12,7 @@ const Main = () => {
         <>
             <MainHeader />
             <MainContentsWrap>
-                <FilterBtnWrap>
-                    <DateFilter />
-                    <PersonFilter />
-                    <ChargeFilter />
-                </FilterBtnWrap>
+                <FilterButtons />
             </MainContentsWrap>
         </>
     )
