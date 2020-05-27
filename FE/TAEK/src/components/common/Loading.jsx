9d6894baba260f09b4e806e7c87b95fa45a36977
sgdg <seungdeng17@gmail.com>
2020-05-27 23:56:21 +0900

    Style: 로딩 position 추가

--- FE/TAEK/src/components/common/Loading.jsx
+++ FE/TAEK/src/components/common/Loading.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 import spiner from 'public/images/earth.svg';
+
+const LoadingWrap = styled.div`
+    .loading-spiner {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+`;
 
 const Loading = () => {
     return (
-        <>
+        <LoadingWrap>
             <img className='loading-spiner' src={spiner} alt="loading-spiner" />
-        </>
+        </LoadingWrap>
     )
 }
 
