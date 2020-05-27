3a0dec2a05e93bcf9d150259d8b75fc814e54315
sgdg <seungdeng17@gmail.com>
2020-05-27 15:03:53 +0900

    [#51] Feat: 날짜 카운터 유틸 추가

--- FE/TAEK/src/utils/util.js
+++ FE/TAEK/src/utils/util.js
@@ -1,1 +1,14 @@
 export const numberComma = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+export const dayCounter = (checkIn, checkOut) => {
+    if (!checkOut) return 1;
+
+    const day = 1000 * 60 * 60 * 24;
+    const _checkIn = new Date(checkIn.year, checkIn.month - 1, checkIn.day);
+    const _checkOut = new Date(checkOut.year, checkOut.month - 1, checkOut.day);
+
+    if (_checkIn.getMonth() === 11 && _checkIn.getdate() > 25) _checkOut.setFullYear(_checkOut.getFullYear() + 1);
+
+    const result = Math.round(_checkOut.getTime() - _checkIn.getTime()) / day;
+    return result.toFixed(0);
+}
