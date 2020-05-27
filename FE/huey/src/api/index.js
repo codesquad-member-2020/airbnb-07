59b2c55e14a89671e7b5f1d11b093d974bacd5d8
Hu2y <sanghun_@daum.net>
2020-05-27 14:18:56 +0900

    [#54] Feat: API index파일에 baseURL 주소 변경

--- FE/huey/src/api/index.js
+++ FE/huey/src/api/index.js
@@ -3,14 +3,14 @@ import { setInterceptors } from './common/interceptors';
 
 function createInstance() {
   return axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+    baseURL: process.env.VUE_APP_BASE_URL,
   });
 }
 
 // 액시오스 초기화 함수
 function createInstanceWithAuth(url) {
   const instance = axios.create({
-    baseURL: `${process.env.VUE_APP_API_URL}${url}`,
+    baseURL: `${process.env.VUE_APP_BASE_URL}${url}`,
   });
   return setInterceptors(instance);
 }
