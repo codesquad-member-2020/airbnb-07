020573318456c569b36a188d67ab3a612ea7db5f
Hu2y <sanghun_@daum.net>
2020-05-27 14:27:12 +0900

    [#54] Feat: 초기랜더링을 위한 RoomLists 컴포넌트 추가

---
+++ FE/huey/src/components/main/RoomLists.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="room-list-wrap">
+    <div v-if="this.isRenderData">loading....</div>
+    <div v-else v-for="data in this.initRenderRooms.allData" :key="data.index">
+      <RoomCard :propsData="data" />
+    </div>
+  </div>
+</template>
+
+<script>
+import RoomCard from '@/components/main/RoomCard';
+import { mapState } from 'vuex';
+
+export default {
+  components: {
+    RoomCard,
+  },
+  computed: {
+    ...mapState(['initRenderRooms']),
+    isRenderData() {
+      if (this.initRenderRooms.hasOwnProperty('allData')) return false;
+      return true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.room-list-wrap {
+  margin-top: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 340px;
+  grid-gap: 35px;
+}
+</style>
