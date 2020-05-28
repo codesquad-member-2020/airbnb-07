eedc66e5bb1639f586642b8e9f11813f484f3c06
Hu2y <sanghun_@daum.net>
2020-05-28 17:16:04 +0900

    [#53] Feat: 예약 모달창에 필요한 state, getters, mutations 추가
     - 금액계산부분을 상태로 추가해주었따
     - 인원수를 상태관리로 추가해 주었다.

--- FE/huey/src/store/index.js
+++ FE/huey/src/store/index.js
@@ -10,12 +10,30 @@ export default new Vuex.Store({
     isOpenModal: false,
     payloadDate: [],
     isPayload: false,
+    checkinDate: '',
+    checkoutDate: '',
+    guestNumber: 0,
+    minPirce: 0,
+    maxPrice: 0,
   },
   getters: {
     isPayloadData(state) {
       if (state.payloadDate.hasOwnProperty('currentPrice'))
         state.isPayload = !state.isPayload;
       return state.isPayload;
+    },
+
+    dateCount(state) {
+      const daysBetween =
+        (Date.parse(state.checkinDate) - Date.parse(state.checkoutDate)) /
+        (24 * 3600 * 1000);
+      return Math.abs(daysBetween);
+    },
+
+    sumPrice(state, getters) {
+      const result =
+        state.payloadDate.currentPrice * getters.dateCount * state.guestNumber;
+      return result;
     },
   },
 
@@ -27,6 +45,15 @@ export default new Vuex.Store({
       state.isOpenModal = !state.isOpenModal;
       if (payload === 'undefined') return;
       state.payloadDate = payload;
+    },
+    setCheckInDate(state, value) {
+      state.checkinDate = value;
+    },
+    setCheckOutDate(state, value) {
+      state.checkoutDate = value;
+    },
+    setGuestNumber(state, number) {
+      state.guestNumber = number;
     },
   },
 
