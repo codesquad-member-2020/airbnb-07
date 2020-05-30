f74ef177234f61d91260ab6dbc846ab7a86ef02b
sgdg <seungdeng17@gmail.com>
2020-05-30 15:18:51 +0900

    [#66] Feat: 마이페이지(예약 정보 조회) 추가

--- FE/TAEK/src/components/myPage/MyPage.jsx
+++ FE/TAEK/src/components/myPage/MyPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainHeader from '@/components/header/MainHeader';
+import ReservationList from './ReservationList';
 
 const MyPage = () => {
     return (
         <>
             <MainHeader />
+            <ReservationList />
         </>
     )
 }
---
+++ FE/TAEK/src/components/myPage/ReservationList.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import styled from 'styled-components';
+import ReservationCard from './ReservationCard';
+
+const ReservationListWrap = styled.div`
+    padding: 40px 15%;
+    table > thead > tr > th {
+        padding: 15px;
+    }
+    table > tbody > tr > td {
+        text-align: center;
+        vertical-align: middle;
+    }
+    table > tbody > tr {
+        border-bottom: 0.5px solid #ecf0f1;
+        :last-child {
+            border-bottom: none;
+        }
+    }
+    thead {
+        border-bottom: 0.5px solid #ecf0f1;
+        font-weight: 600;
+    }
+    .reservation-title {
+        font-size: 20px;
+        font-weight: 600;
+        color: #000;
+        margin-bottom: 40px;
+    }
+    .table-container {
+        box-shadow: ${props => props.theme.modalShadow};
+        border-radius: 5px;
+        margin-top: 35px;
+        width: 100%;
+    }
+    .reservation-card-container {
+        .hotel-container {
+        width: 300px;
+        padding: 20px;
+            img {
+                width: 280px;
+                height: auto;
+                border-radius: 5px;
+                box-shadow: ${props => props.theme.boxShadow};
+            }
+            .hotel-name-title {
+                margin-top: 10px;
+            }
+        }
+        .date-info {
+            display: flex;
+            flex-direction: column;
+            .date-arrow {
+                margin: 5px 0;
+                transform: rotate(90deg);
+            }
+        }
+        .reservation-cancel-btn {
+            width: 60%;
+            height: 35px;
+            color: #fff;
+            border: none;
+            outline: none;
+            background-color: #ff385c;
+            border-radius: 5px;
+            font-size: 16px;
+            font-weight: 600;
+            font-family: 'Noto Sans KR', sans-serif;
+            box-shadow: ${props => props.theme.boxShadow};
+            cursor: pointer;
+        }
+        .cancel-btn-container {
+            width: 100px;
+        }
+    }
+`;
+
+import { reservationList } from 'mock/mockData.js';
+
+const ReservationList = () => {
+    const reservationCards = reservationList[0].allData.map((reservationInfo, index) => {
+        return (
+            <ReservationCard
+                key={reservationInfo.reservations[0].reservationId}
+                {...{ reservationInfo, index }}
+            />)
+    });
+
+    return (
+        <ReservationListWrap>
+            <h2 className="reservation-title">마이페이지</h2>
+            <table className="table-container">
+                <thead>
+                    <tr>
+                        <th width="5%"></th>
+                        <th width="30%">숙박 정보</th>
+                        <th width="20%">날짜</th>
+                        <th width="11%">인원</th>
+                        <th width="14%">금액</th>
+                        <th width="20%">예약 취소</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {reservationCards}
+                </tbody>
+            </table>
+        </ReservationListWrap>
+    )
+}
+
+export default ReservationList
