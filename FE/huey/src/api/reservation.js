808f75afc5aca0fffef3bda08abb3f2ee0c56eeb
Hu2y <sanghun_@daum.net>
2020-05-30 15:54:36 +0900

    [#75] Feat: 예약하기 API 함수 추가

--- FE/huey/src/api/reservation.js
+++ FE/huey/src/api/reservation.js
@@ -9,8 +9,12 @@ function initMainRedner() {
   return instance.get('mock/init');
 }
 
+function setReservation(accommodationId, setData) {
+  return instance.post(`mock/${accommodationId}/guswns1659@gmail.com`, setData);
+}
+
 function reservationInfo() {
   return instance.get('mock/reservationInfo/guswns1659@gmail.com');
 }
 
-export { fetchPosts, initMainRedner, reservationInfo };
+export { fetchPosts, initMainRedner, setReservation, reservationInfo };
