243745c6134a0a95bd0f4722b99bb30b0033e818
sgdg <seungdeng17@gmail.com>
2020-06-01 21:12:06 +0900

    [#94] Fix: 검색 데이터 요청시 에러처리 안되던 문제 수정

--- FE/TAEK/src/store/modules/rooms/roomsAction.js
+++ FE/TAEK/src/store/modules/rooms/roomsAction.js
@@ -33,10 +33,9 @@ const getRoomsFilterData = data => async dispatch => {
             },
             body: JSON.stringify(data.filterData),
         });
-        const json = await response.json();
-
         if (response.status !== 200) throw (`${response.status}Error! ${COMMON.GET_DATA_ERROR}`);
 
+        const json = await response.json();
         dispatch({ type: GET_ROOMS_SUCCESS, payload: json });
         dispatch(applyChargeFilter(data.min, data.max));
         dispatch(saveFilterData(data.filterData));
